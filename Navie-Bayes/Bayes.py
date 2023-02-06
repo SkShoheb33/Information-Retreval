a92ba3404fc19b8db93aefd5414df06cc20fe604
SkShoheb33 <shaikshoheb9k@gmail.com>
2023-02-06 23:43:32 +0530

    version 1.0.0.3

--- Navie-Bayes/Bayes.py
+++ Navie-Bayes/Bayes.py
@@ -1,37 +1,26 @@
 import pandas as pd
 class Bayes:
-    def __init__(self) -> None:
+    def __init__(self):
         self.data = pd.read_excel('data.xlsx')
-        # print(self.data)
-        self.labels = self.data.columns[1:-1]
-        self.predict = self.data[self.data.columns[-1]].unique()
-        # print("hello" ,self.predict)
-        self.classes = {}
-        for c in self.predict:
-            self.classes[c] = self.countValues(self.data[self.data.columns[-1]],c)
-        # print("hi",self.classes)
         self.tables = {}
-        for lable in self.labels:
-            self.tables[lable] = self.create_table(self.data[lable])
-        # print(self.labels)
-
-    def countValues(self,col,value):
-        # print(col,value)
+        self.table_names = self.data.columns[1:-1]
+        self.predict = self.data.columns[-1]
+        self.predicted_values = self.data[self.predict].unique()
+        print(self.predict)
+        for name in self.table_names:
+            self.createTable(name)
+        # print(self.tables)
+        # for table in self.tables:
+        #     print(self.tables[table])
+    def createTable(self,table_name):
+        unique = self.data[table_name].unique()
         c = 0
-        for i in col:
-            if i == value:
-                c+=1
-        return c
-
-    def create_table(self,column):
-        # print(column)
-        rows = column.unique()
-        cols = self.predict
-        df = {}
-        for row in rows:
-            print(row,column)
-            df[row] = self.countValues(row,column)
-        return df
-        # print(rows,cols)
-        print(df)
+        self.tables[table_name] = pd.DataFrame(data=None,index=unique,columns=self.predicted_values)
+        for row in self.predicted_values: #no yes
+            for col in unique: #rows 
+                self.tables[table_name][row][col] = len(
+                    self.data[(self.data[table_name]==col) & (self.data[self.predict]==row)]
+                    )/len(self.data[self.data[self.predict]==row])    
+    # def classifyQuery(self,query):
+        
 obj = Bayes()
