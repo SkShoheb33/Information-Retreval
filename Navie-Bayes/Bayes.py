56d076a4b2ec41f8892cd843f1dc0a70ec6cbec7
SkShoheb33 <shaikshoheb9k@gmail.com>
2023-02-07 09:56:51 +0530

    version 1.0.0.4

--- Navie-Bayes/Bayes.py
+++ Navie-Bayes/Bayes.py
@@ -6,12 +6,8 @@ class Bayes:
         self.table_names = self.data.columns[1:-1]
         self.predict = self.data.columns[-1]
         self.predicted_values = self.data[self.predict].unique()
-        print(self.predict)
         for name in self.table_names:
             self.createTable(name)
-        # print(self.tables)
-        # for table in self.tables:
-        #     print(self.tables[table])
     def createTable(self,table_name):
         unique = self.data[table_name].unique()
         c = 0
@@ -21,6 +17,18 @@ class Bayes:
                 self.tables[table_name][row][col] = len(
                     self.data[(self.data[table_name]==col) & (self.data[self.predict]==row)]
                     )/len(self.data[self.data[self.predict]==row])    
-    # def classifyQuery(self,query):
-        
+    def classifyQuery(self,query):
+        query = query.split()
+        result = 0
+        m = 0
+        for c in self.predicted_values:
+            a = 1
+            for k,i in zip(self.tables,query):
+                a *= self.tables[k][c][i]
+            if m<a:
+                m = a
+                result = c
+        print(result)
+            
 obj = Bayes()
+obj.classifyQuery("Sunny Hot High Strong")
